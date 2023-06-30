ab0528c30b0c9f63963cd8cab0fdc461e33ca620
Arnim <arnime@kpn-cert.nl>
2023-06-30 18:52:31 +0200

    Formatting

--- commands/tweetfeed/command.py
+++ commands/tweetfeed/command.py
@@ -37,15 +37,14 @@ def process(command, channel, username, params):
             with requests.get(APIENDPOINT, headers=headers) as response:
                 json_response = response.json()
                 if len(json_response):
-                    entries = []
+                    message = '\n'
+                    message += '\n| Date | User | Type | Value | Tags | URL |'
+                    message += '\n| :- | :- | :- | :- | :- | :- |'
+                    message += '\n'
                     for entry in json_response:
                         if params in entry['value']:
                             if not len(messages):
                                 messages.append({'text': 'Tweetfeed search results for `%s`:' % (params,)})
-                                message = '\n'
-                                message += '\n| Date | User | Type | Value | Tags | URL |'
-                                message += '\n| :- | :- | :- | :- | :- | :- |'
-                                message += '\n'
                             for k in ('date', 'user', 'type', 'value', 'tags'):
                                 if k in entry:
                                     if isinstance(entry[k], list):
