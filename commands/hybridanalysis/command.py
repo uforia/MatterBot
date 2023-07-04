4983a24c754c53671944e686230b7faaabc59843
Arnim <arnime@kpn-cert.nl>
2023-07-04 22:17:48 +0200

    Improve Hybrid-Analysis support

--- commands/hybridanalysis/command.py
+++ commands/hybridanalysis/command.py
@@ -26,119 +26,137 @@ def process(command, channel, username, params):
     try:
         if len(params)>0:
             query = regex.sub('',params[0].lower())
-            querytype = None
+            endpoints = None
             if re.search(r"^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(\:[0-65535]*)?$", query):
-                endpoint = 'search/terms'
-                querytype = 'host'
+                endpoints = {
+                    'host': 'search/terms',
+                }
             elif re.search(r"^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))", query):
-                endpoint = 'search/terms'
-                querytype = 'host'
+                endpoints = {
+                    'host': 'search/terms',
+                }
             elif re.search(r"^(((?!\-))(xn\-\-)?[a-z0-9\-_]{0,61}[a-z0-9]{1,1}\.)*(xn\-\-)?([a-z0-9\-]{1,61}|[a-z0-9\-]{1,30})\.[a-z]{2,}$", query):
-                endpoint = 'search/terms'
-                querytype = 'domain'
+                endpoints = {
+                    'domain': 'search/terms',
+                }
             elif 'http' in query:
-                endpoint = 'search/terms'
-                querytype = 'url'
+                endpoints = {
+                    'url': 'search/terms',
+                }
             elif re.search(r"^[A-Fa-f0-9]{32}$", query):
-                endpoint = 'search/hash'
-                querytype = 'hash'
+                endpoints = {
+                    'hash': 'search/hash',
+                    'similar_to': 'search/terms',
+                    'context': 'search/terms',
+                }
             elif re.search(r"^[A-Fa-f0-9]{40}$", query):
-                endpoint = 'search/hash'
-                querytype = 'hash'
+                endpoints = {
+                    'hash': 'search/hash',
+                    'similar_to': 'search/terms',
+                    'context': 'search/terms',
+                }
             elif re.search(r"^[A-Fa-f0-9]{64}$", query):
-                endpoint = 'search/hash'
-                querytype = 'hash'
+                endpoints = {
+                    'hash': 'search/hash',
+                    'similar_to': 'search/terms',
+                    'context': 'search/terms',
+                    'imphash': 'search/terms',
+                    'authentihash': 'search/terms',
+                }
             elif re.search(r"((\d*):(\w*):(\w*)|(\d*):(\w*)\+(\w*):(\w*))", query):
-                endpoint = 'search/terms'
-                querytype = 'ssdeep'
+                endpoints = {
+                    'ssdeep': 'search/terms',
+                }
             else:
-                endpoint = 'search/terms'
-                querytype = 'vx_family'
-            if querytype:
+                endpoints = {
+                    'vx_family': 'search/terms',
+                }
+            if endpoints:
                 headers = {
                     'User-Agent': 'VxApi CLI Connector',
                     'api-key': '%s' % settings.APIURL['hybridanalysis']['key'],
                 }
-                data = {querytype:query}
-                APIENDPOINT = settings.APIURL['hybridanalysis']['url']+endpoint
-                with requests.post(APIENDPOINT, data=data, headers=headers) as response:
-                    message = ''
-                    json_response = response.json()
-                    if len(json_response):
-                        count = 0
-                        # Deal with /search/hash output
-                        if endpoint == 'search/hash':
-                            for entry in json_response:
-                                count += 1
-                                message = '| Hybrid-Analysis Result %s | `%s` |' % (count,query)
-                                message += '\n| -: | :- |'
-                                singlefields = {
-                                    'submit_name': 'Filename',
-                                    'verdict': 'Verdict',
-                                    'analysis_start_time': 'Submitted at',
-                                    'type': 'Filetype',
-                                    'size': 'Filesize',
-                                    'md5': 'MD5 hash',
-                                    'sha1': 'SHA1 hash',
-                                    'sha256': 'SHA256 hash',
-                                    'ssdeep': 'SSDEEP hash',
-                                    'imphash': 'Imphash',
-                                    'vx_family': 'Malware family',
-                                }
-                                multifields = {
-                                    'tags': 'Tags',
-                                    'classification_tags': 'Classifications',
-                                    'domains': 'Domains',
-                                    'compromised_hosts': 'Compromised hosts',
-                                    'hosts': 'Hosts',
-                                    'extracted_files': 'Extracted files',
-                                    'processes': 'Processes',
-                                    'mitre_attcks': 'MITRE ATT&CK TTPs',
-                                    'signatures': 'Signatures',
-                                }
-                                for singlefield in singlefields:
-                                    if singlefield in entry:
-                                        value = entry[singlefield]
-                                        if value:
-                                            message += '\n| %s | `%s` |' % (singlefields[singlefield], entry[singlefield])
-                                for multifield in multifields:
-                                    if multifield in entry:
-                                        value = entry[multifield]
-                                        if value:
-                                            message += '\n| %s | `%s` |' % (multifields[multifield], '`, `'.join(entry[multifield]))
-                                message += '\n\n'
-                                messages.append({'text': message})
-                        # Deal with /search/terms output
-                        if endpoint == 'search/terms':
-                            if 'result' in json_response:
-                                for entry in json_response['result']:
+                for endpoint in endpoints:
+                    data = {endpoint:query}
+                    APIENDPOINT = settings.APIURL['hybridanalysis']['url']+endpoints[endpoint]
+                    with requests.post(APIENDPOINT, data=data, headers=headers) as response:
+                        message = ''
+                        json_response = response.json()
+                        if len(json_response):
+                            count = 0
+                            # Deal with /search/hash output
+                            if endpoints[endpoint] == 'search/hash':
+                                for entry in json_response:
                                     count += 1
-                                    # Only display the first five results for now
-                                    if count>3:
-                                        messages.append({
-                                            'text': 'More than 3 results ('+str(len(json_response['result']))+'), attaching complete JSON.',
-                                            'uploads': [
-                                                {'filename': 'hybridanalysis-'+querytype+'-'+datetime.datetime.now().strftime('%Y%m%dT%H%M%S')+'.json', 'bytes': response.content}
-                                            ]
-                                            })
-                                        break
-                                    message = '| Hybrid-Analysis Result %s | `%s` |' % (count,query)
+                                    message = '| Hybrid-Analysis `%s` Result %s | `%s` |' % (endpoint, count,query)
                                     message += '\n| -: | :- |'
                                     singlefields = {
-                                        'submit_name': 'Submitted Name',
+                                        'submit_name': 'Filename',
                                         'verdict': 'Verdict',
                                         'analysis_start_time': 'Submitted at',
+                                        'type': 'Filetype',
+                                        'size': 'Filesize',
+                                        'md5': 'MD5 hash',
+                                        'sha1': 'SHA1 hash',
                                         'sha256': 'SHA256 hash',
+                                        'ssdeep': 'SSDEEP hash',
+                                        'imphash': 'Imphash',
+                                        'authentihash': 'Authentihash',
                                         'vx_family': 'Malware family',
+                                    }
+                                    multifields = {
+                                        'tags': 'Tags',
+                                        'classification_tags': 'Classifications',
+                                        'domains': 'Domains',
+                                        'compromised_hosts': 'Compromised hosts',
+                                        'hosts': 'Hosts',
+                                        'extracted_files': 'Extracted files',
+                                        'processes': 'Processes',
+                                        'mitre_attcks': 'MITRE ATT&CK TTPs',
+                                        'signatures': 'Signatures',
                                     }
                                     for singlefield in singlefields:
                                         if singlefield in entry:
                                             value = entry[singlefield]
                                             if value:
                                                 message += '\n| %s | `%s` |' % (singlefields[singlefield], entry[singlefield])
+                                    for multifield in multifields:
+                                        if multifield in entry:
+                                            value = entry[multifield]
+                                            if value:
+                                                message += '\n| %s | `%s` |' % (multifields[multifield], '`, `'.join(entry[multifield]))
                                     message += '\n\n'
                                     messages.append({'text': message})
-
+                            # Deal with /search/terms output
+                            if endpoints[endpoint] == 'search/terms':
+                                if 'result' in json_response:
+                                    for entry in json_response['result']:
+                                        count += 1
+                                        # Only display the first five results for now
+                                        if count>3:
+                                            messages.append({
+                                                'text': 'More than 3 results ('+str(len(json_response['result']))+'), attaching complete JSON.',
+                                                'uploads': [
+                                                    {'filename': 'hybridanalysis-'+querytype+'-'+datetime.datetime.now().strftime('%Y%m%dT%H%M%S')+'.json', 'bytes': response.content}
+                                                ]
+                                                })
+                                            break
+                                        message = '| Hybrid-Analysis `%s` Result %s | `%s` |' % (endpoint, count,query)
+                                        message += '\n| -: | :- |'
+                                        singlefields = {
+                                            'submit_name': 'Submitted Name',
+                                            'verdict': 'Verdict',
+                                            'analysis_start_time': 'Submitted at',
+                                            'sha256': 'SHA256 hash',
+                                            'vx_family': 'Malware family',
+                                        }
+                                        for singlefield in singlefields:
+                                            if singlefield in entry:
+                                                value = entry[singlefield]
+                                                if value:
+                                                    message += '\n| %s | `%s` |' % (singlefields[singlefield], entry[singlefield])
+                                        message += '\n\n'
+                                        messages.append({'text': message})
     except Exception as e:
         messages.append({'text': 'A Python error occurred searching Hybrid-Analysis:\nError:' + str(e)})
     finally:
