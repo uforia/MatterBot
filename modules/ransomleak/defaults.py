489b72d148ada83d830b165bcf5184be57aba612
Arnim <arnime@kpn-cert.nl>
2023-07-14 18:55:55 +0200

    RansomLeak module

---
+++ modules/ransomleak/defaults.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+NAME = "RansomLeak"
+CHANNEL = "news"
+URL = "<URL-to-JSONs>"
+AUTH = {
+    'username': None,
+    'password': None,
+}
+GROUPS = (
+    'Lorenz',
+)
+ENTRIES = 10
---
+++ modules/ransomleak/feed.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+# Every module must set the CHANNEL variable to indicate where information should be sent to in Mattermost
+#
+# Every module must implement the query() function.
+# This query() function is called by the main worker and has only one parameter: the number of historic
+# items that should be returned in the list.
+#
+# Every module must return a list [...] with 0, 1 ... n entries
+# of 2-tuples: ('<channel>', '<content>')
+#
+# <channel>: basically the destination channel in Mattermost, e.g. 'Newsfeed', 'Incident', etc.
+# <content>: the content of the message, MD format possible
+
+import requests
+import yaml
+from pathlib import Path
+try:
+    from modules.ransomleak import defaults as settings
+except ModuleNotFoundError: # local test run
+    import defaults as settings
+    if Path('settings.py').is_file():
+        import settings
+else:
+    if Path('modules/ransomleak/settings.py').is_file():
+        try:
+            from modules.ransomleak import settings
+        except ModuleNotFoundError: # local test run
+            import settings
+
+def query(MAX=settings.ENTRIES):
+    items = []
+    if settings.AUTH['username'] and settings.AUTH['password']:
+        authentication = (settings.AUTH['username'],settings.AUTH['password'])
+    try:
+        for group in settings.GROUPS:
+            ENDPOINT = settings.URL+group+'.json'
+            with requests.get(ENDPOINT, auth=authentication) as response:
+                feed = yaml.safe_load(response.content)
+            entries = feed[-MAX:]
+            for entry in entries:
+                group = entry['group']
+                date = entry['published'] if 'published' in entry else 'N/A'
+                victim = entry['company']
+                size = entry['size'] if 'size' in entry else 'an unknown amount'
+                content = settings.NAME + ': Actor **%s**' % (group,)
+                content += ' has leaked `%s` of data' % (size,)
+                content += ' from victim **%s**' % (victim,)
+                if date:
+                    content += ' at `%s`' % (date,)
+                items.append([settings.CHANNEL, content])
+    except Exception as e:
+        content = "An error occurred during the Ransomleaks feed parsing."
+        items.append([settings.CHANNEL,content])
+    finally:
+        return items
+
+if __name__ == "__main__":
+    print(query())
