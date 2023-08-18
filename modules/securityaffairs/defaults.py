10cd6dd3a505b70db0cf82ae3cfa5eb87842fc77
Arnim <arnime@kpn-cert.nl>
2023-08-18 12:07:17 +0200

    Epic derp.. Actually add files this time

---
+++ modules/securityaffairs/defaults.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+NAME = "Security Affairs"
+CHANNELS = (
+    "newsfeed",
+)
+URL = "https://securityaffairs.com/wordpress/rss"
+ENTRIES = 10
---
+++ modules/tripwire/defaults.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+NAME = "Tripwire State of Security"
+CHANNELS = (
+    "newsfeed",
+)
+URL = "https://www.tripwire.com/state-of-security/feed"
+ENTRIES = 10
