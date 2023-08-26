45ef69bbea8d3dc58d17451f40fcb756b2b0de7f
Arnim <arnime@kpn-cert.nl>
2023-08-26 20:40:51 +0200

    Add TheHackerNews

---
+++ modules/hackernews/defaults.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+NAME = "The Hacker News"
+CHANNELS = (
+    "newsfeed",
+)
+URL = [
+    "https://hnrss.org/frontpage",
+    "https://hnrss.org/newest?q=security",
+]
+ENTRIES = 30
