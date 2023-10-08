b3dfbb8f4ebd2e26e49fb9813b4c3913c0d7ad71
Arnim <arnime@kpn-cert.nl>
2023-10-08 14:42:08 +0200

    Add GreyNoise functionality

--- commands/greynoise/command.py
+++ commands/greynoise/command.py
@@ -43,7 +43,7 @@ def process(command, channel, username, params, files, conn):
     messages = []
     try:
         if len(params)>0:
-            querytype = params[0] if params[0] in querytypes else 'community'
+            querytype = params[0].lower() if params[0] in querytypes else 'community'
             if not querytype in querytypes:
                 return
             APIENDPOINT = settings.APIURL['greynoise']['url']
@@ -60,7 +60,7 @@ def process(command, channel, username, params, files, conn):
                             expiration = json_response['expiration']
                             offering = json_response['offering']
                             message =  '\n| **GreyNoise** | `Account Information` |'
-                            message += '\n| :- | -: |'
+                            message += '\n| :- | :- |'
                             message += '\n| **Offering** | `%s` |' % (offering,)
                             message += '\n| **Expiration** | `%s` |' % (expiration,)
                             message += '\n\n'
@@ -81,7 +81,7 @@ def process(command, channel, username, params, files, conn):
                             json_response = response.json()
                             if response.status_code in (200,404):
                                 message =  '\n| **GreyNoise** Lookup Type | `%s` |' % (querytype)
-                                message += '\n| :- | -: |'
+                                message += '\n| :- | :- |'
                                 fields = {
                                     'ip': 'IP Address',
                                     'noise': 'Noise',
@@ -197,6 +197,141 @@ def process(command, channel, username, params, files, conn):
                                                 message += '\n| **%s** | `%s` |' % (fields[field],value)
                                 message += '\n\n'
                                 messages.append({'text': message})
+                    if querytype == 'ipquick':
+                        query = query[0].strip()
+                        APIENDPOINT += urllib.parse.quote(querytypes[querytype]+'%s' % (query,))
+                        with requests.get(APIENDPOINT, headers=headers) as response:
+                            json_response = response.json()
+                            if response.status_code == 200:
+                                codemap = {
+                                    '0x00': 'The IP has never been observed scanning the Internet',
+                                    '0x01': 'The IP has been observed by the GreyNoise sensor network',
+                                    '0x02': 'The IP has been observed scanning the GreyNoise sensor network, but has not completed a full connection, meaning this can be spoofed',
+                                    '0x03': 'The IP is adjacent to another host that has been directly observed by the GreyNoise sensor network',
+                                    '0x04': 'Reserved (unknown)',
+                                    '0x05': 'This IP is commonly spoofed in Internet-scan activity',
+                                    '0x06': 'This IP has been observed as noise, but this host belongs to a cloud provider where IPs can be cycled frequently',
+                                    '0x07': 'This IP is invalid',
+                                    '0x08': 'This IP was classified as noise, but has not been observed engaging in Internet-wide scans or attacks in over 90 days',
+                                    '0x09': 'This IP was found in RIOT',
+                                    '0x10': 'The IP has been observed by the GreyNoise sensor network and was found in RIOT',
+                                }
+                                message =  '\n| **GreyNoise** Lookup Type | `%s` |' % (querytype)
+                                message += '\n| :- | :- |'
+                                fields = {
+                                    'code': 'Status',
+                                    'ip': 'IP Address',
+                                    'noise': 'Noise',
+                                    'riot': 'RIOT',
+                                }
+                                for field in fields:
+                                    if field in json_response:
+                                        value = json_response[field]
+                                        if field == 'code':
+                                            if value in codemap:
+                                                value = codemap[value]
+                                            else:
+                                                value = 'Unknown status code'
+                                        message += '\n| **%s** | `%s` |' % (fields[field],value)
+                                message += '\n\n'
+                                messages.append({'text': message})
+                    if querytype == 'riot':
+                        query = query[0].strip()
+                        APIENDPOINT += urllib.parse.quote(querytypes[querytype]+'%s' % (query,))
+                        with requests.get(APIENDPOINT, headers=headers) as response:
+                            json_response = response.json()
+                            if response.status_code in (200,404):
+                                message =  '\n| **GreyNoise** Lookup Type | `%s` |' % (querytype)
+                                message += '\n| :- | :- |'
+                                fields = {
+                                    'ip': 'IP Address',
+                                    'riot': 'RIOT',
+                                }
+                                for field in fields:
+                                    if field in json_response:
+                                        value = json_response[field]
+                                        if field == 'riot':
+                                            value = ':white_check_mark: `(benign)`' if value else ':x: `(not in RIOT)`'
+                                            message += '\n| **%s** | %s |' % (fields[field],value)
+                                        else:
+                                            message += '\n| **%s** | `%s` |' % (fields[field],value)
+                                message += '\n\n'
+                                messages.append({'text': message})
+                    if querytype == 'timeline':
+                        filtertype = query[0].strip().lower().split(':')[0]
+                        filtervalues = query[0].strip().lower().split(':')[1:]
+                        filtertypes = {
+                            'daily': {
+                                'endpoint': 'daily-summary',
+                                'filters': {
+                                    'days': 1,
+                                    'cursor': None,
+                                    'limit': 50,
+                                },
+                            },
+                            'hourly': {
+                                'endpoint': 'hourly-summary',
+                                'filters': {
+                                    'days': 1,
+                                    'cursor': None,
+                                    'limit': 50,
+                                },
+                            },
+                            'field': {
+                                'endpoint': 'timeline',
+                                'filters': {
+                                    'days': 1,
+                                    'field': [
+                                        'destination_port',
+                                        'http_path',
+                                        'http_user_agent',
+                                        'source_asn',
+                                        'source_org',
+                                        'tag_ids',
+                                        'classification',
+                                    ],
+                                    'granularity': '1d',
+                                },
+                            },
+                        }
+                        if filtertype in filtertypes:
+                            filterdict = {}
+                            queryfilter = filtertypes[filtertype]
+                            query = query[1].strip()
+                            APIENDPOINT += urllib.parse.quote(querytypes[querytype]+'%s' % (query,))
+                            APIENDPOINT += '/'+urllib.parse.quote(queryfilter['endpoint'])+'?'
+                            if len(filtervalues):
+                                filtervalues = ':'.join(filtervalues).split(',')
+                            for filtervalue in filtervalues:
+                                name, value = filtervalue.split(':')
+                                filterdict[name] = value
+                            for field in queryfilter['filters']:
+                                if field not in filterdict:
+                                    if field != 'field':
+                                        if queryfilter['filters'][field]:
+                                            filterdict[field] = queryfilter['filters'][field]
+                            for field in filterdict:
+                                APIENDPOINT += '%s=' % (field,)
+                                APIENDPOINT += urllib.parse.quote('%s' % (filterdict[field],))
+                                APIENDPOINT += '&'
+                            APIENDPOINT = APIENDPOINT.strip('&')
+                            with requests.get(APIENDPOINT, headers=headers) as response:
+                                json_response = response.json()
+                                if 'message' in json_response:
+                                    if json_response['message'] == 'Forbidden':
+                                        messages.append({'text': 'Your API key does not have access to the GreyNoise timeline functionality.'})
+                                else:
+                                    print(json_response) # I cannot complete this functionality without a working API key, unfortunately
+                    if querytype == 'similarity':
+                        query = query[0].strip()
+                        APIENDPOINT += urllib.parse.quote(querytypes[querytype]+'%s' % (query,))
+                        with requests.get(APIENDPOINT, headers=headers) as response:
+                            json_response = response.json()
+                            if 'message' in json_response:
+                                if json_response['message'] == 'Forbidden':
+                                    messages.append({'text': 'Your API key does not have access to the GreyNoise similarity functionality.'})
+                            else:
+                                print(json_response) # I cannot complete this functionality without a working API key, unfortunately
     except Exception as e:
         messages.append({'text': 'A Python error occurred searching GreyNoise:\nError: `%s`' % (traceback.format_exc(),)})
     finally:
