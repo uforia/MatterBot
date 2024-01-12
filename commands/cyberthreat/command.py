168c762a403c845a31be60e0a04bcf2b615106f5
Tycho <tycho@cyberthreat.nl>
2024-01-12 16:00:00 +0100

    Poc of formatted messsages. Formatting done in command module.

--- commands/cyberthreat/command.py
+++ commands/cyberthreat/command.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import re
-import requests
+import logging
 import tldextract
 from pathlib import Path
 from datetime import datetime
@@ -31,26 +31,87 @@ actorlist = dict()
 for actor in results['results']:
     actorlist[actor['name']]=actor
 
+"""
+Input data in the format
+
+{
+    "source":"provider",
+    "responses": [
+        {
+            "paragraph":"subtitle",
+            "preamble":"introduction to source",
+            "data": [
+                {"category":"category", "datapoint":"datapoint", "stixtype":"ipv4-addr", "value":"value"},
+                {"category":"category", "datapoint":"datapoint", "value":"value"}
+            ]
+        }
+    ]
+}
+
+Converts to a message text and possibly an attachment.
+The text can have multiple paragraph with a short introduction
+
+"""
+
+def formatdata(data):
+    logging.debug(f"working with {data}")
+    text = f"### {data['source']}\n"
+    if 'intro' in data:
+         text += f"{data['intro']}\n"
+    for response in data['responses']:
+        text += f"#### {response['paragraph']}\n"
+        if 'preamble' in response:
+            text += response['preamble']
+            text +="\n\n"
+        if 'data' in response:
+            text += "|Category | Datapoint | Value|\n| :- | -: | :- |\n"
+            logging.warning(f"{response['data']}")
+            for line in response['data']:
+                text += f"|{line['category']}|{line['datapoint']}|{line['value']}\n"
+    logging.debug(f"Text to return: {text}")
+    return text
+
+
+
 
 def process(command, channel, username, params, files, conn):
     filters = '&'.join(settings.APIURL['cyberthreat']['filters'])
 
 
     if len(params)>0:
-        params = params[0].replace('[', '').replace(']', '').replace('hxxp','http').lower()
-        intro = f"cyberthreat.nl *Hosting Intelligence* API search for `{params}`:"
+        params = params[0].replace('[', '').replace(']', '').replace('hxxp','http').lower().strip()
+        data = { "source":"cyberthreat hosting intelligence",
+                    "responses": []
+}
+        
+        data['intro'] = f"cyberthreat.nl *Hosting Intelligence* API search for `{params}`:"
         listitem = '`\n- `'
         try:
-
             if params in actorlist:
                 text = f"**{actorlist[params]['name'].capitalize()}**\n{actorlist[params]['description']}"
+                data['responses'].append({})
+                data['responses'][0]['paragraph'] = "Bulletproof hosting provider"
+                data['responses'][0]['preamble']  = actorlist[params]['description']
+                data['responses'][0]['data'] = list()
+                data['responses'][0]['data'].append({"category":"Actor", "datapoint":"name", "stixtype":"", "value":actorlist[params]['name']})
+                data['responses'][0]['data'].append({"category":"Actor", "datapoint":"type", "stixtype":"", "value":actorlist[params]['type']})
+
 
             elif re.search(r"^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(\:[0-65535]*)?$", params):
                 results = cyberthreat.wget('addresses/'+params+'?'+filters)
                 for address in results:
                     last_seen = datetime.strptime(address['last_seen'], '%Y-%m-%dT%H:%M:%S.%f%z')
-                    text=f"IPv4 address `{params}` {settings.confidence_tabel[address['credibility']]['level']} used by the actor **{address['actor'].capitalize()}**.\n"
-                    text+=f"Last seen: {last_seen.strftime('%Y-%m-%d')}"
+                    data['responses'].append(dict())
+                    data['responses'][0]['paragraph'] = "IP Lookup"
+                    data['responses'][0]['preamble']=f"IPv4 address `{params}` {settings.confidence_tabel[address['credibility']]['level']} used by the actor **{address['actor'].capitalize()}**.\n"
+                    data['responses'][0]['data'] = list()
+                    data['responses'][0]['data'].append({"category":"Indicator", "datapoint":"ipv4 address", "stixtype":"ipv4-addr", "value":params})
+                    data['responses'][0]['data'].append({"category":"Indicator", "datapoint":"last seen", "stixtype":"", "value":last_seen.strftime('%Y-%m-%d')})
+                    data['responses'][0]['data'].append({"category":"Indicator", "datapoint":"actor", "stixtype":"", "value":address['actor'].capitalize() })
+                    data['responses'][0]['data'].append({"category":"Indicator", "datapoint":"actor type", "stixtype":"", "value":address['type'].capitalize() })
+                    data['responses'][0]['data'].append({"category":"Indicator", "datapoint":"credibility", "stixtype":"", "value": settings.confidence_tabel[address['credibility']]['short_description']})
+
+            
             elif params:
                 extract = tldextract.extract(params)
                 extracted_domain = extract.registered_domain
@@ -75,6 +136,7 @@ def process(command, channel, username, params, files, conn):
                         fqdnlist[domain]['actor'] = result.get('actor')
                         fqdnlist[domain]['type']  = result.get('type')
                     
+                    logging.warning(f"fqdn list: {fqdnlist}")
                     if len(fqdnlist):
                         text='The domainname '
 
@@ -83,23 +145,37 @@ def process(command, channel, username, params, files, conn):
                         """
                         for domain in fqdnlist:
                             text+=f"`{domain}` {settings.confidence_tabel[fqdnlist[domain]['credibility']]['level']} hosted on the {fqdnlist[domain]['type']} network of actor **{fqdnlist[domain]['actor'].capitalize()}**.\n"
-                            text+=f"Last seen: {fqdnlist[domain]['last_seen'].strftime('%Y-%m-%d')}.\n"
-                            if len(fqdnlist[domain]['subdomains']):
-                                text+=f"We have found the following subdomains: \n- `{listitem.join(fqdnlist[domain]['subdomains'])}`."
-                        
+
+                            data['responses'].append({})
+                            data['responses'][0]['paragraph'] = "Domain search"
+                            data['responses'][0]['preamble']  = text
+                            data['responses'][0]['data'] = list()
+                            data['responses'][0]['data'].append({"category":"Hosting", "datapoint":"domain", "stixtype":"", "value":domain})
+                            data['responses'][0]['data'].append({"category":"Hosting", "datapoint":"actor", "stixtype":"", "value":fqdnlist[domain]['actor']})
+                            data['responses'][0]['data'].append({"category":"Hosting", "datapoint":"credibility", "stixtype":"", "value":settings.confidence_tabel[fqdnlist[domain]['credibility']]['short_description']})
+                            data['responses'][0]['data'].append({"category":"Hosting", "datapoint":"last seen", "stixtype":"", "value":fqdnlist[domain]['last_seen'].strftime('%Y-%m-%d')})
+                            for item in fqdnlist[domain]['subdomains']:
+                                data['responses'][0]['data'].append({"category":"Domain", "datapoint":"fqdn", "stixtype":"", "value":item})
+
+
                 else:
                     """ In case the params doesnt even look like a valid domain name. """
                     return
 
-            if 'text' in locals():
-                return {'messages': [
-                    {'text': intro + '\n' + text},
-                ]}
-            #else:
-            #    return {'messages': [
-            #        {'text': 'cyberthreat API searched for `%s` without result' % (params.strip(),)}
-            #    ]}
+            if len(data['responses']):
+                text = formatdata(data)
+                logging.warning(f"Returned text: {text}")
+                return  {'messages': [
+                            {'text': text} 
+                    ]   }
+            
+            else:
+               return {'messages': [
+                   {'text': 'cyberthreat API searched for `%s` without result' % (params.strip(),)}
+               ]}
         except Exception as e:
+            raise e
             return {'messages': [
                 {'text': 'An error occurred searching cyberthreat for `%s`:\nError: `%s`' % (params, e)},
             ]}
+            
