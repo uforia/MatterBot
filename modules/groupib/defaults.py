175c1cd4ce95a152be97d7794a47731b3ba20b66
Arnim <arnime@kpn-cert.nl>
2024-04-26 15:12:55 +0200

    Added Group-IB and TheRecord

---
+++ modules/groupib/defaults.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+NAME = "Group-IB Blog"
+CHANNELS = (
+    "newsfeed",
+)
+URL = "https://blog.group-ib.com/rss.xml"
+ENTRIES = 10
---
+++ modules/therecord/defaults.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+NAME = "The Record Media"
+CHANNELS = (
+    "newsfeed",
+)
+URL = "https://therecord.media/news/cybercrime/feed"
+ENTRIES = 10
