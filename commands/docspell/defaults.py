2cb21d64d2feec39eda250b558f7e6e88efa19f8
Arnim <uforia@dhcp.net>
2025-02-04 21:36:04 +0100

    Textual clarifications

--- commands/docspell/defaults.py
+++ commands/docspell/defaults.py
@@ -17,19 +17,19 @@ CONTENTTYPE = 'application/json'
 HELP = {
     'DEFAULT': {
         'args': '<search> or <upload>',
-        'desc': 'You can either <search> through the Docspell user\'s collective or <upload> new content to it. If '
+        'desc': 'You can either <search> through the DMS\' user\'s collective or <upload> new content to it. If '
                 'you do not specify the query type, <search> is assumed.',
     },
     'search': {
         'args': '<keyword1>, <keyword2> ... <keyword#>',
-        'desc': 'Search through the Docspell user\'s collective for the given keywords. Searches are performed '
-                'exactly as if you were using the Docspell Web UI, meaning parentheses, logical operators, etc. '
+        'desc': 'Search through the DMS user\'s collective for the given keywords. Searches are performed '
+                'exactly as if you were using the DMS\' Web UI, meaning parentheses, logical operators, etc. '
                 'will work as intended.',
     },
     'upload': {
         'args': '<NONE>',
-        'desc': 'Upload new content to the Docspell user\'s collective. If in a supported format such as text, '
-                'Office documents, PDFs, etc., Docspell will immediately process the new file and extract its '
+        'desc': 'Upload new content to the DMS. If in a supported format such as text, '
+                'Office documents, PDFs, etc., the DMS will immediately process the new file and extract its '
                 'contents.',
     },
 }
