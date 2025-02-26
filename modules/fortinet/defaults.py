97292847929a23e88336bb39f7f590b090a00f02
mrKell0gs <juriaan.denijs@kpn.com>
2025-02-26 15:07:09 +0100

    Add fortinet rss feeds
    PSIRT blogs and Threat research from fortinet. Test with multiple rss urls in one feed, therefore in seperate commit.

---
+++ modules/fortinet/defaults.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+NAME = "Fortinet PSIRT/Threat Research Blogs"
+CHANNELS = (
+    "newsfeed",
+)
+URLS = "https://feeds.fortinet.com/fortinet/blog/threat-research", "https://feeds.fortinet.com/fortinet/blog/psirt"
+ENTRIES = 10
