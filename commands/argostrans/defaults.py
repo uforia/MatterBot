8f8d255cad5c2a4b867f7316078ac5861a53d5e2
mrKell0gs <juriaan.denijs@kpn.com>
2025-03-08 15:51:29 +0100

    Add Argostranslate
    Added a seperate translation command which is already being used by the jpcert rss feed. Users can input a source language which translates a given string to the default destination language (English). Allowed translation pairs are dynamically pulled from the [argospm index repo](https://github.com/argosopentech/argospm-index/blob/main/index.json).

---
+++ commands/argostrans/defaults.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+BINDS = ['@translate', '@argos']
+CHANS = ['debug']
+
+PACKAGES = 'https://raw.githubusercontent.com/argosopentech/argospm-index/main/index.json'
+DEFAULT_LAN = 'en'
+
+HELP = {
+    'DEFAULT': {
+        'args': '<source lan> <dest lan (optional)> <string to translate>',
+        'desc': 
+        f'Argos Translate is a offline translation library. It uses OpenNMT for translations. To view available language use the `-h` flag. \
+        \nArgos takes a source language as argument and tries to translate that to the default language (`{DEFAULT_LAN}`) or specified language. \
+        \nExample usage: `@argos ja こんにちは` or `@argos en ja Hello`'
+    },
+}
