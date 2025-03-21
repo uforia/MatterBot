54081d64e2855f2832209b5fbf2d6d1d166bdfb5
Arnim <arnime@kpn-cert.nl>
2025-03-21 09:21:18 +0100

    Typo

--- commands/argostrans/defaults.py
+++ commands/argostrans/defaults.py
@@ -10,7 +10,7 @@ HELP = {
     'DEFAULT': {
         'args': '<source lan> <dest lan (optional)> <string to translate>',
         'desc': 'Argos Translate is a offline translation library. It uses OpenNMT for translations. To view available language use the `-h` flag. '
-                'Argos takes a source language as argument and tries to translate that to the default language (`{DEFAULT_LAN}`) or specified language. ' 
+                'Argos takes a source language as argument and tries to translate that to the default language (`{DEFAULT_LAN}`) or specified language. '
                 'Example usage: `@argos ja こんにちは` or `@argos en ja Hello`.',
     },
 }
