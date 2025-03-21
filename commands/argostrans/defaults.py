6c68a4760f66426fe3d43cd6a84aef51e1f7c7b2
Arnim <arnime@kpn-cert.nl>
2025-03-21 09:13:59 +0100

    Fix help

--- commands/argostrans/defaults.py
+++ commands/argostrans/defaults.py
@@ -9,9 +9,8 @@ DEFAULT_LAN = 'en'
 HELP = {
     'DEFAULT': {
         'args': '<source lan> <dest lan (optional)> <string to translate>',
-        'desc': 
-        f'Argos Translate is a offline translation library. It uses OpenNMT for translations. To view available language use the `-h` flag. \
-        \nArgos takes a source language as argument and tries to translate that to the default language (`{DEFAULT_LAN}`) or specified language. \
-        \nExample usage: `@argos ja こんにちは` or `@argos en ja Hello`'
+        'desc': 'Argos Translate is a offline translation library. It uses OpenNMT for translations. To view available language use the `-h` flag. '
+                'Argos takes a source language as argument and tries to translate that to the default language (`{DEFAULT_LAN}`) or specified language. ' 
+                'Example usage: `@argos ja こんにちは` or `@argos en ja Hello`.',
     },
 }
