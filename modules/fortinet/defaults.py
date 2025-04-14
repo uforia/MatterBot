3951c8bdfd9f84feba6249ac6895b12007b419f9
mrKell0gs <juriaan.denijs@kpn.com>
2025-04-14 17:57:17 +0200

    Extend filter

--- modules/fortinet/defaults.py
+++ modules/fortinet/defaults.py
@@ -4,12 +4,13 @@ NAME = "Fortinet PSIRT/Threat Research Blogs"
 CHANNELS = (
     "newsfeed",
 )
-URLS =  ( 
-    "https://filestore.fortinet.com/fortiguard/rss/threatsignal.xml",
-    "https://feeds.fortinet.com/fortinet/blog/threat-research",
-    "https://filestore.fortinet.com/fortiguard/rss/ir.xml",
+URLS =  (
     "https://feeds.fortinet.com/fortinet/blog/psirt", 
+    "https://filestore.fortinet.com/fortiguard/rss/ir.xml",
     "https://filestore.fortinet.com/fortiguard/rss/outbreakalert.xml",
 )
+
+# "https://filestore.fortinet.com/fortiguard/rss/threatsignal.xml",
+#     "https://feeds.fortinet.com/fortinet/blog/threat-research",
 
 ENTRIES = 10
--- modules/sploitus/feed.py
+++ modules/sploitus/feed.py
@@ -17,9 +17,7 @@ import feedparser
 import os
 import requests
 import sys
-import traceback
 from pathlib import Path
-
 
 try:
     from modules.sploitus import defaults as settings
@@ -35,32 +33,32 @@ else:
             import settings
 
 def importScore():
-    # Import dynamic threshold score from opencve module
     running = os.path.abspath(__file__)
     cwd = os.path.abspath(os.path.join(os.path.dirname(running), '..'))
     if cwd not in sys.path:
         sys.path.insert(0, cwd)
-    from opencve.defaults import ADVISORYTHRESHOLD
+    from opencve.defaults import ADVISORYTHRESHOLD # Import threshold score from opencve module
     return ADVISORYTHRESHOLD
 
 def checkPage(link):
     try:
         with requests.Session() as session: 
-            response = session.get(link, headers={'User-Agent': 'MatterBot RSS Automation 1.0'})
+            response = session.get(link, headers={
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                })
+                # 'User-Agent': 'MatterBot RSS Automation 1.0'
             response.raise_for_status()
             data = bs4.BeautifulSoup(response.content, "html.parser")
             matches = data.select('div.tile-subtitle.text-gray') # Check if feed url contains CVSS property on page 
-    except requests.exceptions.RequestException as e:
+    except requests.exceptions.RequestException:
         matches = False
-        print({f"An HTTP error occurred querying Sploitus:\nError: {str(e)}\n{traceback.format_exc()}"})
     return matches
-
 
 def query(MAX=settings.ENTRIES):
     items = []
     feed = feedparser.parse(settings.URL, agent='MatterBot RSS Automation 1.0')
     count = 0
-    stripchars = '`\\[\\]\'\"'
     while count < MAX:
         try:
             title = feed.entries[count].title[:-8] # Remove standard 'exploit' str from title
@@ -68,13 +66,14 @@ def query(MAX=settings.ENTRIES):
             THRESHOLD = importScore()
             matches = checkPage(link)
             filtered = False
-            try:
-                cvss = ''.join([score.text.strip()[-3:] for score in matches])
-            except NameError:
+            if not matches:
                 cvss = 'N/A'
-            for score in matches:
-                if float(score.text.strip()[-3:]) > THRESHOLD: # Check if CVSS score meets threshold
-                    filtered = True
+                filtered = True
+            else:
+                cvss = ''.join([score.text.strip()[-3:] for score in matches])
+                for score in matches:
+                    if float(score.text.strip()[-3:]) >= THRESHOLD: # Check if CVSS score meets threshold
+                        filtered = True
             if filtered:
                 content = settings.NAME + ': [' + title + f' - CVSS: `{cvss}`' + '](' + link + ')'                
             # TODO: query opencve api and get description.
