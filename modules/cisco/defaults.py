259c367d7a0f6f85a2e9a4bfc664df551420cc40
mrKell0gs <juriaan.denijs@kpn.com>
2025-06-02 10:35:48 +0200

    Add new feeds

---
+++ modules/cisco/defaults.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+NAME = "Cisco Security Advisories"
+CHANNELS = (
+    "newsfeed",
+)
+URL = "https://sec.cloudapps.cisco.com/security/center/psirtrss20/CiscoSecurityAdvisory.xml"
+ENTRIES = 10
---
+++ modules/recordedfuture/defaults.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+NAME = "Recorded Future Threat Research"
+CHANNELS = (
+    "newsfeed",
+)
+URL = "https://www.recordedfuture.com/feed"
+ENTRIES = 10
