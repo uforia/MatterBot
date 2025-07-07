b005faae0030069104a8e489af7906974cc91a1c
Arnim <arnime@kpn-cert.nl>
2025-07-07 13:41:05 +0200

    Mastodon support

---
+++ modules/mastodon/defaults.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+NAME = "Mastodon Social Network"
+URLS = {
+    'Kevin Beaumont':           'https://cyberplace.social/@GossiTheDog.rss',
+    'Troy Hunt':                'https://infosec.exchange/@troyhunt.rss',
+    'Auonsson':                 'https://mastodon.world/@auonsson.rss',
+}
+CHANNELS = (
+    "newsfeed",
+)
+TOPICS = (
+    "General",
+)
+ADMIN_ONLY = False
+ENTRIES = 10
